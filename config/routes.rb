dab4cb48b05e5143aa3cadde0f614c01bfcccd58
Ryan Michael <ryan@bcarc.com>
2010-02-04 20:30:40 -0600

    blocking out routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,6 @@
 # map.namespace :admin do |admin|
 #   admin.resources :whatever
 # end  
+
+map.gc_index '/c/', :controller => :group_collections, :action => :index
+map.gc_search '/c/:group_collection_name', :controller => :group_collections, :action => :show
