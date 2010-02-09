b3b634172cabfb0fcb7d92fc00692d7a6b17972c
Ryan Michael <ryan@bcarc.com>
2010-02-09 09:39:13 -0600

    removing file eliminated by last commit

--- app/models/group_collection_branch.rb
+++
@@ -1,8 +0,0 @@
-class GroupCollectionBranch < ActiveRecord::Base
-  belongs_to :parent, :class_name => "GroupCollection"
-  belongs_to :child, :class_name => "GroupCollection"
-
-  validates_presence_of :parent
-  validates_presence_of :child
-end
-
